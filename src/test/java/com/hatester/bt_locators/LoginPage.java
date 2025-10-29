cf8537e32e138aca1dd038af9689e442f0757fb2
Tranthuha19982015 <73157358+Tranthuha19982015@users.noreply.github.com>
2025-10-29 16:37:56 +0700

    locator Login Page

---
+++ src/test/java/com/hatester/bt_locators/LoginPage.java
@@ -0,0 +1,17 @@
+package com.hatester.bt_locators;
+
+public class LoginPage {
+    //Locator Login Page
+    public static String headerLoginPage = "//h1[normalize-space()='Login']";
+    public static String inputEmail = "//input[@id='email']";
+    public static String inputPassword = "//input[@id='password']";
+    public static String buttonLogin = "//button[normalize-space()='Login']";
+    public static String checkboxRememberMe = "//input[@id='remember']";
+    public static String linkForgotPassword = "//a[normalize-space()='Forgot Password?']";
+    public static String errorMessageInvalidEmailOrPassword = "//div[@id='alerts']/div[normalize-space()='Invalid email or password']";
+    public static String errorMessageEmailIsNull = "//div[text()='The Email Address field is required.' and contains(@class,'alert-danger')]";
+    public static String errorMessagePasswordIsNull = "//div[text()='The Password field is required.' and contains(@class,'alert-danger')]";
+
+    //locator Menu Dashboard
+    public static String menuDashboard = "//ul[@id='side-menu']//span[normalize-space()='Dashboard' and @class='menu-text']]";
+}
