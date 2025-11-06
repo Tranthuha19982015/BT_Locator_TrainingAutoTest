8c60b16d8fb0023573d6c6b726b5d4ea00953ba1
Tranthuha19982015 <73157358+Tranthuha19982015@users.noreply.github.com>
2025-11-06 11:27:27 +0700

    add url to login page

--- src/test/java/com/hatester/bt_locators/LocatorLoginPage.java
+++ src/test/java/com/hatester/bt_locators/LocatorLoginPage.java
@@ -1,6 +1,8 @@
 package com.hatester.bt_locators;
 
 public class LocatorLoginPage {
+
+    public static String url="https://crm.anhtester.com/admin/authentication";
     //Locator Login Page
     public static String headerLoginPage = "//h1[normalize-space()='Login']";
     public static String inputEmail = "//input[@id='email']";
