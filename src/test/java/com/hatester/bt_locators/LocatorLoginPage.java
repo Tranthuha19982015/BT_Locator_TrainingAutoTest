71492c7f7d056b5b526015a4ebbf09d91d101b41
Tranthuha19982015 <73157358+Tranthuha19982015@users.noreply.github.com>
2025-11-28 10:43:30 +0700

    update object String -> By

--- src/test/java/com/hatester/bt_locators/LocatorLoginPage.java
+++ src/test/java/com/hatester/bt_locators/LocatorLoginPage.java
@@ -6,15 +6,15 @@ public class LocatorLoginPage {
 
     public static String url = "https://crm.anhtester.com/admin/authentication";
     //Locator Login Page
-    public static String headerLoginPage = "//h1[normalize-space()='Login']";
-    public static String inputEmail = "//input[@id='email']";
-    public static String inputPassword = "//input[@id='password']";
-    public static String buttonLogin = "//button[normalize-space()='Login']";
-    public static String checkboxRememberMe = "//input[@id='remember']";
-    public static String linkForgotPassword = "//a[normalize-space()='Forgot Password?']";
-    public static String errorMessageInvalidEmailOrPassword = "//div[@id='alerts']/div[normalize-space()='Invalid email or password']";
-    public static String errorMessageEmailIsNull = "//div[text()='The Email Address field is required.' and contains(@class,'alert-danger')]";
-    public static String errorMessagePasswordIsNull = "//div[text()='The Password field is required.' and contains(@class,'alert-danger')]";
+    public static By headerLoginPage = By.xpath("//h1[normalize-space()='Login']");
+    public static By inputEmail = By.xpath("//input[@id='email']");
+    public static By inputPassword = By.xpath("//input[@id='password']");
+    public static By buttonLogin = By.xpath("//button[normalize-space()='Login']");
+    public static By checkboxRememberMe = By.xpath("//input[@id='remember']");
+    public static By linkForgotPassword = By.xpath("//a[normalize-space()='Forgot Password?']");
+    public static By errorMessageInvalidEmailOrPassword = By.xpath("//div[@id='alerts']/div[normalize-space()='Invalid email or password']");
+    public static By errorMessageEmailRequired = By.xpath("//div[text()='The Email Address field is required.' and contains(@class,'alert-danger')]");
+    public static By errorMessagePasswordRequired = By.xpath("//div[text()='The Password field is required.' and contains(@class,'alert-danger')]");
 
     //locator Menu Dashboard
     public static By menuDashboard = By.xpath("//ul[@id='side-menu']//span[normalize-space()='Dashboard' and @class='menu-text']");
