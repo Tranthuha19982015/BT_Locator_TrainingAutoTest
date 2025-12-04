7bf2a7db5264aa1b4bf60d2850e1f622c997f834
Tranthuha19982015 <Tranthuha19982015@gmail.com>
2025-12-04 16:59:55 +0700

    add code locator header and clickLogout on BasePage

--- src/test/java/com/hatester/common/BasePage.java
+++ src/test/java/com/hatester/common/BasePage.java
@@ -3,6 +3,7 @@ package com.hatester.common;
 import com.hatester.keywords.WebUI;
 import com.hatester.pages.DashboardPage;
 import com.hatester.pages.LeadPage;
+import com.hatester.pages.LoginPage;
 import com.hatester.pages.TaskPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,9 +16,18 @@ public class BasePage {
         this.driver = driver;
     }
 
+    //menu
     public By menuDashboard = By.xpath("//ul[@id='side-menu']//span[normalize-space()='Dashboard' and @class='menu-text']");
+    public By menuCustomers = By.xpath("//ul[@id='side-menu']//span[normalize-space()='Customers' and @class='menu-text']");
+    public By menuProjects = By.xpath("//ul[@id='side-menu']//span[normalize-space()='Projects' and @class='menu-text']");
     public By menuLead = By.xpath("//ul[@id='side-menu']//span[normalize-space()='Leads' and @class='menu-text']");
     public By menuTasks = By.xpath("//ul[@id='side-menu']//span[normalize-space()='Tasks' and @class='menu-text']");
+
+    //header
+    public By iconProfile = By.xpath("//div[@id='header']/descendant::li[contains(@class,'header-user-profile')]");
+    public By linkMyProfile = By.xpath("//div[@id='header']/descendant::a[text()='My Profile']");
+    public By linkEditProfile = By.xpath("//div[@id='header']/descendant::a[text()='Edit Profile']");
+    public By linkLogout = By.xpath("//div[@id='header']/descendant::a[text()='Logout']");
 
     public DashboardPage clickMenuDashboard() {
         WebUI.clickElement(driver, menuDashboard);
@@ -32,5 +42,11 @@ public class BasePage {
     public TaskPage clickMenuTask() {
         WebUI.clickElement(driver, menuTasks);
         return new TaskPage(driver);
+    }
+
+    public LoginPage clickLogout() {
+        WebUI.clickElement(driver, iconProfile);
+        WebUI.clickElement(driver, linkLogout);
+        return new LoginPage(driver);
     }
 }
