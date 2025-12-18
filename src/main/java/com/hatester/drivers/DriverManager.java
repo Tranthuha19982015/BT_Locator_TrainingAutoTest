f4784cc90db9ec137a348fa56172bf0d2c6e5888
Tranthuha19982015 <Tranthuha19982015@gmail.com>
2025-12-18 15:05:34 +0700

    thêm comment giải thích code class DriverManager

--- src/main/java/com/hatester/drivers/DriverManager.java
+++ src/main/java/com/hatester/drivers/DriverManager.java
@@ -3,21 +3,21 @@ package com.hatester.drivers;
 import org.openqa.selenium.WebDriver;
 
 public class DriverManager {
-    public static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    public static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();  //final định nghĩa hằng, hằng thì không thể gán lại giá trị
 
     private DriverManager() {
     }
 
     public static WebDriver getDriver() {
-        return driver.get();
+        return driver.get(); //lấy driver theo từng luồng
     }
 
     public static void setDriver(WebDriver driver) {
-        DriverManager.driver.set(driver);
+        DriverManager.driver.set(driver); //set driver theo đúng luồng
     }
 
     public static void quitDriver() {
-        driver.get().quit();
-        driver.remove();
+        driver.get().quit();  //tắt driver, reset giá trị driver = null, quit() thuộc Selenium
+        driver.remove();  //xóa luồng, xóa luôn vùng nhớ trong bộ nhớ máy tính, remove() thuộc ThreadLocal
     }
 }
