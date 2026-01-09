41dfb9cdfb89148a9d00846720877a5666fd87e3
Tranthuha19982015 <Tranthuha19982015@gmail.com>
2026-01-09 15:31:30 +0700

    add code lấy ngày giờ hiện tại

--- src/main/java/com/hatester/helpers/SystemHelper.java
+++ src/main/java/com/hatester/helpers/SystemHelper.java
@@ -1,11 +1,31 @@
 package com.hatester.helpers;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class SystemHelper {
     //Lấy ra đường dẫn từ ổ đĩa tới source code hiện tại của mình  ---   E:\BT_Locator_TrainingAutoTest
     public static String getCurrentDir() {
         String current = System.getProperty("user.dir") + File.separator;
         return current;
+    }
+
+    public static String getCurrentDatetime() {
+        return new SimpleDateFormat("_ddMMyyyy_HHmmss").format(new Date());
+    }
+
+    public static String getDateTimeNowFormat() {
+        //Lấy thời gian hiện tại
+        LocalDateTime now = LocalDateTime.now();
+
+        //Định dạng ngày giờ theo pattern
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        //Chuyển sang chuỗi và thay thế ký tự
+        String formatted = now.format(formatter).replace("-", "_").replace(":", "_").replace(" ", "_");
+        return formatted;
     }
 }
